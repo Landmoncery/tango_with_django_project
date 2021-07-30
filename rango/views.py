91444724700ac282d5ef338aec565d182220d3b8
Luyao Ma <2598647m@student.gla.ac.uk>
2021-07-30 10:30:45 +0100

    image

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+#    return HttpResponse("Rango says hey there partner! <br/><a href='/rango/about/'>About</a>")
 #<br/> <a href = '/rango/about/'>About</a>
 
 def about(request):
